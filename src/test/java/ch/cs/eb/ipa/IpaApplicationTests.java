2dbaa075f360313fa85c7cd8da7f008cef950dd9
elijonberisha <51161691+elijonberisha@users.noreply.github.com>
2022-03-10 15:42:28 +0100

    BIBA-1 I have set up the base project using start.spring.io. The project does not have any functionalities yet. It runs on port 8080.

---
+++ src/test/java/ch/cs/eb/ipa/IpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.cs.eb.ipa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
