7f1fcb3b70cc4b0a42baff47a50ca8230de0c5a8
elijonberisha <51161691+elijonberisha@users.noreply.github.com>
2022-03-14 17:23:20 +0100

    BIPA-8 I have implemented the activation feature. The activation feature simply changes the role of a user, if he does not have the EMPLOYEE role.

--- src/main/java/ch/cs/eb/ipa/controller/admin/AdminUserManagementController.java
+++ src/main/java/ch/cs/eb/ipa/controller/admin/AdminUserManagementController.java
@@ -9,6 +9,8 @@ import ch.cs.eb.ipa.util.UsernameFetcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -36,6 +38,48 @@ public class AdminUserManagementController {
         }
         model.addAttribute("self", userRepository.getByCtsId(Integer.parseInt(usernameFetcher.getUsername())));
         model.addAttribute("criteria", new SearchCriteriaContainer());
+
+        for (CUser c : userList) {
+            if (c.getUser_authority().getRole().equals(UserAuthority.INACTIVE)) {
+                inactiveUsers.add(c);
+            }
+        }
+
+        model.addAttribute("users", inactiveUsers);
+
+        if (inactiveUsers.isEmpty()) {
+            model.addAttribute("message", "There are no inactive users.");
+            return "user_management";
+        }
+
+        return "user_management";
+    }
+
+    @GetMapping("activate_profile/{id}")
+    public String activateProfile(@PathVariable("id") int id, Model model) {
+        if (userRepository.getByCtsId(id) == null) {
+            return "redirect:/user_management";
+        }
+
+        UsernameFetcher usernameFetcher = new UsernameFetcher();
+
+        List<CUser> userList = userRepository.fetchAllUsers();
+        List<CUser> inactiveUsers = new ArrayList<>();
+
+        if (userRepository.getByCtsId(Integer.parseInt(usernameFetcher.getUsername())).getUser_authority().getRole().equals(UserAuthority.ADMIN)) {
+            model.addAttribute("admin", "no relevance");
+        }
+        model.addAttribute("self", userRepository.getByCtsId(Integer.parseInt(usernameFetcher.getUsername())));
+        model.addAttribute("criteria", new SearchCriteriaContainer());
+
+        if (userRepository.getByCtsId(id).getUser_authority().getRole().equals(UserAuthority.INACTIVE)) {
+            CUser inactiveUser = userRepository.getByCtsId(id);
+            inactiveUser.setUser_authority(authorityRepository.getByAuthorityEnum(UserAuthority.EMPLOYEE));
+            userRepository.updateUser(inactiveUser, inactiveUser.getId());
+            model.addAttribute("activation", "User " + inactiveUser.getCts_id() + " has been activated!");
+        } else {
+            model.addAttribute("activation", "User is already activated.");
+        }
 
         for (CUser c : userList) {
             if (c.getUser_authority().getRole().equals(UserAuthority.INACTIVE)) {
