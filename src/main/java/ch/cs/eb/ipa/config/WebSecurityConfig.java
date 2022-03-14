e8a2d003978222384d15533ec2c72407c480d74f
elijonberisha <51161691+elijonberisha@users.noreply.github.com>
2022-03-14 12:21:53 +0100

    BIPA-4 I have implemented the login process. The login is functinal, yet the redirections after the login are yet to be implementd. Registration is yet to be implemented. I have fixed a formatting error in the login template, the submit button is now displayed on the right side.

--- src/main/java/ch/cs/eb/ipa/config/WebSecurityConfig.java
+++ src/main/java/ch/cs/eb/ipa/config/WebSecurityConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -33,5 +35,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.authenticationProvider(daoAuthenticationProvider());
     }
 
-    
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").defaultSuccessUrl("/home").permitAll();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        http.sessionManagement().maximumSessions(1).expiredUrl("/expired").and().invalidSessionUrl("/expired");
+    }
 }
