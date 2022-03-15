2f286da8d30f3453d41609e6b88c46b47530181d
elijonberisha <51161691+elijonberisha@users.noreply.github.com>
2022-03-15 13:02:02 +0100

    BIPA-10 I have implemented the new function in the UrlChecker class, that checks if a url is valid via regex. I have replaced all usages of isUrl with isUrlRegex in the EditProfileController. Load times are now reduced significantly.

--- src/main/java/ch/cs/eb/ipa/controller/profile/EditProfileController.java
+++ src/main/java/ch/cs/eb/ipa/controller/profile/EditProfileController.java
@@ -56,78 +56,78 @@ public class EditProfileController {
         model.addAttribute("youtube", new Youtube());
 
         if (degreed.getArticleLink() != null && !degreed.getArticleLink().trim().equals("") && degreed.getArticleLink().length() > 1) {
-            if (!urlChecker.isUrl(degreed.getArticleLink()) || !degreed.getArticleLink().contains("degreed.com")) {
+            if (!urlChecker.isUrlRegex(degreed.getArticleLink()) || !degreed.getArticleLink().contains("degreed.com")) {
                 model.addAttribute("message", "Your Degreed link is invalid. Please reconsider your input.");
                 return "edit_profile";
             }
         }
 
         if (oreilly.getBookLink() != null && !oreilly.getBookLink().trim().equals("") && oreilly.getBookLink().length() > 1) {
-            if (!urlChecker.isUrl(oreilly.getBookLink()) || !oreilly.getBookLink().contains("oreilly.com")) {
+            if (!urlChecker.isUrlRegex(oreilly.getBookLink()) || !oreilly.getBookLink().contains("oreilly.com")) {
                 model.addAttribute("message", "Your O'Reilly link is invalid. Please reconsider your input.");
                 return "edit_profile";
             }
         }
 
         if (youtube.getTutorialLink() != null && !youtube.getTutorialLink().trim().equals("") && youtube.getTutorialLink().length() > 1) {
-            if (!urlChecker.isUrl(youtube.getTutorialLink()) || !youtube.getTutorialLink().contains("youtube.com")) {
+            if (!urlChecker.isUrlRegex(youtube.getTutorialLink()) || !youtube.getTutorialLink().contains("youtube.com")) {
                 model.addAttribute("message", "Your YouTube link is invalid. Please reconsider your input.");
                 return "edit_profile";
             }
         }
 
         if (ted.getTalk1Link() != null && !ted.getTalk1Link().trim().equals("") && ted.getTalk1Link().length() > 1) {
-            if (!urlChecker.isUrl(ted.getTalk1Link()) || !ted.getTalk1Link().contains("ted.com")) {
+            if (!urlChecker.isUrlRegex(ted.getTalk1Link()) || !ted.getTalk1Link().contains("ted.com")) {
                 model.addAttribute("message", "Your first TED link is invalid. Please reconsider your input.");
                 return "edit_profile";
             }
         }
         if (ted.getTalk2Link() != null && !ted.getTalk2Link().trim().equals("") && ted.getTalk2Link().length() > 1) {
-            if (!urlChecker.isUrl(ted.getTalk2Link()) || !ted.getTalk2Link().contains("ted.com")) {
+            if (!urlChecker.isUrlRegex(ted.getTalk2Link()) || !ted.getTalk2Link().contains("ted.com")) {
                 model.addAttribute("message", "Your second TED link is invalid. Please reconsider your input.");
                 return "edit_profile";
             }
         }
         if (ted.getTalk3Link() != null && !ted.getTalk3Link().trim().equals("") && ted.getTalk3Link().length() > 1) {
-            if (!urlChecker.isUrl(ted.getTalk3Link()) || !ted.getTalk3Link().contains("ted.com")) {
+            if (!urlChecker.isUrlRegex(ted.getTalk3Link()) || !ted.getTalk3Link().contains("ted.com")) {
                 model.addAttribute("message", "Your third TED link is invalid. Please reconsider your input.");
                 return "edit_profile";
             }
         }
 
         if (w3schools.getArticle1Link() != null && !w3schools.getArticle1Link().trim().equals("") && w3schools.getArticle1Link().length() > 1) {
-            if (!urlChecker.isUrl(w3schools.getArticle1Link()) || !w3schools.getArticle1Link().contains("w3schools.com")) {
+            if (!urlChecker.isUrlRegex(w3schools.getArticle1Link()) || !w3schools.getArticle1Link().contains("w3schools.com")) {
                 model.addAttribute("message", "Your first W3Schools link is invalid. Please reconsider your input.");
                 return "edit_profile";
             }
         }
         if (w3schools.getArticle2Link() != null && !w3schools.getArticle2Link().trim().equals("") && w3schools.getArticle2Link().length() > 1) {
-            if (!urlChecker.isUrl(w3schools.getArticle2Link()) || !w3schools.getArticle2Link().contains("w3schools.com")) {
+            if (!urlChecker.isUrlRegex(w3schools.getArticle2Link()) || !w3schools.getArticle2Link().contains("w3schools.com")) {
                 model.addAttribute("message", "Your second W3Schools link is invalid. Please reconsider your input.");
                 return "edit_profile";
             }
         }
         if (w3schools.getArticle3Link() != null && !w3schools.getArticle3Link().trim().equals("") && w3schools.getArticle3Link().length() > 1) {
-            if (!urlChecker.isUrl(w3schools.getArticle3Link()) || !w3schools.getArticle3Link().contains("w3schools.com")) {
+            if (!urlChecker.isUrlRegex(w3schools.getArticle3Link()) || !w3schools.getArticle3Link().contains("w3schools.com")) {
                 model.addAttribute("message", "Your third W3Schools link is invalid. Please reconsider your input.");
                 return "edit_profile";
             }
         }
 
         if (udemy.getTraining1Link() != null && !udemy.getTraining1Link().trim().equals("") && udemy.getTraining1Link().length() > 1) {
-            if (!urlChecker.isUrl(udemy.getTraining1Link()) || !udemy.getTraining1Link().contains("cognizant.udemy.com")) {
+            if (!urlChecker.isUrlRegex(udemy.getTraining1Link()) || !udemy.getTraining1Link().contains("cognizant.udemy.com")) {
                 model.addAttribute("message", "Your first Udemy link is invalid. Please reconsider your input.");
                 return "edit_profile";
             }
         }
         if (udemy.getTraining2Link() != null && !udemy.getTraining2Link().trim().equals("") && udemy.getTraining2Link().length() > 1) {
-            if (!urlChecker.isUrl(udemy.getTraining2Link()) || !udemy.getTraining2Link().contains("cognizant.udemy.com")) {
+            if (!urlChecker.isUrlRegex(udemy.getTraining2Link()) || !udemy.getTraining2Link().contains("cognizant.udemy.com")) {
                 model.addAttribute("message", "Your second Udemy link is invalid. Please reconsider your input.");
                 return "edit_profile";
             }
         }
         if (udemy.getTraining3Link() != null && !udemy.getTraining3Link().trim().equals("") && udemy.getTraining3Link().length() > 1) {
-            if (!urlChecker.isUrl(udemy.getTraining3Link()) || !udemy.getTraining3Link().contains("cognizant.udemy.com")) {
+            if (!urlChecker.isUrlRegex(udemy.getTraining3Link()) || !udemy.getTraining3Link().contains("cognizant.udemy.com")) {
                 model.addAttribute("message", "Your third Udemy link is invalid. Please reconsider your input.");
                 return "edit_profile";
             }
