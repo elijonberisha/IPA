54dbbc867b8dcd4f6fc21456e24ee6e103cdf22e
elijonberisha <51161691+elijonberisha@users.noreply.github.com>
2022-03-15 11:16:08 +0100

    BIPA-11 I have edited the RequestMapping for the change_password template. The template is now fully rendered when called. Back-end implementation is yet to be done.

--- src/main/java/ch/cs/eb/ipa/controller/password/ChangePasswordController.java
+++ src/main/java/ch/cs/eb/ipa/controller/password/ChangePasswordController.java
@@ -1,13 +1,30 @@
 package ch.cs.eb.ipa.controller.password;
 
+import ch.cs.eb.ipa.model.FormContainerUser;
+import ch.cs.eb.ipa.model.UserAuthority;
+import ch.cs.eb.ipa.repository.CUserRepository;
+import ch.cs.eb.ipa.util.UsernameFetcher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ChangePasswordController {
+    @Autowired
+    CUserRepository userRepository;
 
     @RequestMapping("/change_password")
-    public String changePassword() {
+    public String changePassword(Model model) {
+        UsernameFetcher usernameFetcher = new UsernameFetcher();
+
+        if (userRepository.getByCtsId(Integer.parseInt(usernameFetcher.getUsername())).getUser_authority().getRole().equals(UserAuthority.ADMIN)) {
+            model.addAttribute("admin", "no relevance");
+        }
+
+        model.addAttribute("user", new FormContainerUser());
+        model.addAttribute("self", userRepository.getByCtsId(Integer.parseInt(usernameFetcher.getUsername())));
+        
         return "change_password";
     }
 }
