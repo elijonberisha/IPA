74e898d039d251fd92a136e56f030e1320966ecc
elijonberisha <51161691+elijonberisha@users.noreply.github.com>
2022-03-15 12:11:20 +0100

    BIPA-10 I have fully implemented the template rendering for the edit_profile template. I have also modified the column widhts in the edit_profile template, better responsiveness.

--- src/main/java/ch/cs/eb/ipa/controller/profile/EditProfileController.java
+++ src/main/java/ch/cs/eb/ipa/controller/profile/EditProfileController.java
@@ -1,12 +1,36 @@
 package ch.cs.eb.ipa.controller.profile;
 
+import ch.cs.eb.ipa.entity.*;
+import ch.cs.eb.ipa.model.UserAuthority;
+import ch.cs.eb.ipa.repository.CUserRepository;
+import ch.cs.eb.ipa.util.UsernameFetcher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class EditProfileController {
+    @Autowired
+    CUserRepository userRepository;
+
     @RequestMapping("/edit_profile")
-    public String editProfile() {
+    public String editProfile(Model model) {
+        UsernameFetcher usernameFetcher = new UsernameFetcher();
+        CUser user = userRepository.getByCtsId(Integer.parseInt(usernameFetcher.getUsername()));
+
+        if (userRepository.getByCtsId(Integer.parseInt(usernameFetcher.getUsername())).getUser_authority().getRole().equals(UserAuthority.ADMIN)) {
+            model.addAttribute("admin", "no relevance");
+        }
+
+        model.addAttribute("self", user);
+        model.addAttribute("degreed", new Degreed());
+        model.addAttribute("oreilly", new OReilly());
+        model.addAttribute("ted", new Ted());
+        model.addAttribute("udemy", new Udemy());
+        model.addAttribute("w3schools", new W3schools());
+        model.addAttribute("youtube", new Youtube());
+
         return "edit_profile";
     }
 }
