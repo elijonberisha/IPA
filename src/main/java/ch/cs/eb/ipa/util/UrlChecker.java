43ba2da3f295dd4a3b44b97be36b74dadcc97a7e
elijonberisha <51161691+elijonberisha@users.noreply.github.com>
2022-03-16 16:48:33 +0100

    BIPA-15 I have commented the explanation of relevant code lines in the classes of the util package.

--- src/main/java/ch/cs/eb/ipa/util/UrlChecker.java
+++ src/main/java/ch/cs/eb/ipa/util/UrlChecker.java
@@ -10,9 +10,13 @@ import java.net.URL;
  * class: UrlChecker.java
  */
 
+// CLASS IS USED TO VALIDATE THE USER INPUTS IN FORM OF A URL
 public class UrlChecker {
+
+    // CHECKS IF URL CAN BE OPENED; MAKES REQUEST TO URL
     public boolean isUrl(String url) {
         try {
+            // REQUEST IS MADE; IF ABLE TO CONNECT -> TRUE | OTHERWISE -> FALSE
             (new URL(url)).openStream().close();
             return true;
         } catch (MalformedURLException e) {
@@ -23,7 +27,9 @@ public class UrlChecker {
         return false;
     }
 
+    // CHECKS IF URL MATCHES A URL REGEX PATTERN; NO REQUEST IS MADE TO URL
     public boolean isUrlRegex(String url) {
+        // IF URL MATCHES REGEX -> TRUE | OTHERWISE -> FALSE
         if (url.matches("((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[-;:&=\\+\\$,\\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\\+\\$,\\w]+@)[A-Za-z0-9.-]+)((?:\\/[\\+~%\\/.\\w-_]*)?\\??(?:[-\\+=&;%@.\\w_]*)#?(?:[\\w]*))?)")) {
             return true;
         } else {
--- src/main/java/ch/cs/eb/ipa/util/UsernameFetcher.java
+++ src/main/java/ch/cs/eb/ipa/util/UsernameFetcher.java
@@ -11,12 +11,16 @@ import org.springframework.security.core.userdetails.UserDetails;
  * class: UsernameFetcher.java
  */
 
+// CLASS IS USED TO GET INFORMATION ON THE CURRENTLY LOGGED IN USER
 public class UsernameFetcher {
     public String getUsername() {
+        // GETS THE SPRING SECURITY AUTHENTICATION INSTANCE FROM CONTEXT HOLDER, WHICH STORES AUTHENTICATION DATA
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // IF THE AUTHENTICATION IS NULL OR USER IS ANONYMOUS, NO USERNAME IS RETURNED
         if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
             return "";
         } else {
+            // USERNAME IS SET AND RETURNED FOR USAGE
             Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
             String username;
 
